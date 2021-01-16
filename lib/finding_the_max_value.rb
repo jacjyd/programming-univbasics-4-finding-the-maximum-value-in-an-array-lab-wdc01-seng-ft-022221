ec5c70505fa78e7fcb833b8da379691d2414f976
Diana Liao <dijali@gmail.com>
2021-01-16 20:20:45 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,12 +1,12 @@
 def find_max_value(array)
-  # Add your solution here
+  
   currentmax = 0 
   counter = 0
-  while array[counter] do
-    if array[counter] > currentmax
+  #while array[counter] do
+  #  if array[counter] > currentmax
       currentmax = array[counter]
-    end
-  counter += 1
-  end 
+  #  end
+  #counter += 1
+  #end 
   currentmax
 end
